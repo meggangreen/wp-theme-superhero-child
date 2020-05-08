78fb505528c2ff4ac1be344a5de480e913dfc6be
Meggan M Green <gitlab@meggan.green>
2020-05-08 16:26:35 +0000

    Changed tagline text
    Made day quantity 'final' and removed current country, which represents my return
    to the USA. Also required a db update: exit timestamp for final USA entry.

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
  * @package Superhero
  * @since Superhero 1.0
  * @child Meggan Green
- * edited 2017-03-11
+ * edited 2020-05-07
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
@@ -27,56 +27,51 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site"><a class="anchor-jump" id="ptop"></a>
-    <?php do_action( 'before' ); ?>
+<div id="page" class="hfeed site">
+  <a class="anchor-jump" id="ptop"></a>
+  <?php do_action( 'before' ); ?>
 
-    <div id="masthead-wrap">
+  <div id="masthead-wrap">
     <header id="masthead" class="site-header" role="banner">
-        <hgroup class="masthead-table">
-
-            <!-- no site header img call -->
-            <!-- no site logo img call -->
-
-            <table>
-              <tr>
-                <td width="80px">
-                  <img src="http://www.meggangreen.com/maaa/wpf/wp-content/uploads/MA.png" width="70" height="70" class="site-logo" />
-                </td>
-                <td>
-                  <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-
-                <?php   // calls function for number of days and current country
-                    $maaa_immedstats = maaa_stats_header(); ?>
-                    <h2 class="site-immedstats">Meggan left the USA <?php echo $maaa_immedstats[0]; ?> ago and is currently in <?php echo $maaa_immedstats[1]; ?>.</h2>
-
-
-                </td>
-              </tr>
-              <tr>
-                <td colspan="2" align="right">
-
-                  <nav class="site-navigation main-navigation" role="navigation">
-                      <h1 class="assistive-text"><?php _e( 'Menu', 'superhero' ); ?></h1>
-                      <div class="assistive-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'superhero' ); ?>"><?php _e( 'Skip to content', 'superhero' ); ?></a></div>
-                      <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-                  </nav><!-- .site-navigation .main-navigation -->
-
-                </td>
-              </tr>
-            </table>
-
-        </hgroup>
-
-        <div class="clearfix"></div>
+      <hgroup class="masthead-table">
+        <table>
+          <tr>
+            <td width="80px">
+              <img src="http://www.meggangreen.com/maaa/wpf/wp-content/uploads/MA.png"
+                   width="70" height="70" class="site-logo" />
+            </td>
+            <td>
+              <h1 class="site-title">
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+                   title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
+                   rel="home"><?php bloginfo( 'name' ); ?></a>
+              </h1>
+              <!-- Get current days tally and country -->
+              <?php $maaaDays = maaa_stats_header()[0]; ?>
+              <h2 class="site-immedstats">Meggan lived abroad for <?php echo $maaaDays; ?>.She'd like to do it again.</h2>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2" align="right">
+              <nav class="site-navigation main-navigation" role="navigation">
+                  <h1 class="assistive-text"><?php _e( 'Menu', 'superhero' ); ?></h1>
+                  <div class="assistive-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'superhero' ); ?>"><?php _e( 'Skip to content', 'superhero' ); ?></a></div>
+                  <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+              </nav><!-- .site-navigation .main-navigation -->
+            </td>
+          </tr>
+        </table>
+      </hgroup>
+      <div class="clearfix"></div>
     </header><!-- #masthead.site-header -->
-    </div><!-- #masthead-wrap -->
+  </div><!-- #masthead-wrap -->
 
-    <div class="clearfix"></div>
+  <div class="clearfix"></div>
 
-    <?php
+  <?php
     if ( is_front_page() ) :
         get_template_part( 'slider' );
     endif;
-    ?>
+  ?>
 
-    <div id="main" class="site-main">
+  <div id="main" class="site-main">
